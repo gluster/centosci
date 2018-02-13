3b05b815efec9d104339160030dedcad01611635
Nigel Babu <nigelb@redhat.com>
2018-02-13 11:47:35 +0530

    Copy script from path for bootstrap.sh

--- scripts/common/bootstrap.sh
+++ scripts/common/bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -e
 set -x
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no centos-ci/scripts/$1 root@$(cat $WORKSPACE/hosts):$1
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1 root@$(cat $WORKSPACE/hosts):$1
 if [ -z $2 ];
 then
     ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) ./$1
