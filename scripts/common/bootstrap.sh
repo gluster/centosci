e868b660a29eb3e8f46a581610ed8a3a07bc395a
Nigel Babu <nigelb@redhat.com>
2018-03-19 11:57:00 +0530

    Fix bootstrap script

--- scripts/common/bootstrap.sh
+++ scripts/common/bootstrap.sh
@@ -2,10 +2,11 @@
 
 set -e
 set -x
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1 root@$(cat $WORKSPACE/hosts):$1
+EXEC_BIN="$(basename $1)"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$1" "root@$(cat $WORKSPACE/hosts):$EXEC_BIN"
 if [ -z $2 ];
 then
-    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) ./$1
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "root@$(cat $WORKSPACE/hosts)" "./$EXEC_BIN"
 else
-    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) $2 ./$1
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "root@$(cat $WORKSPACE/hosts)" "$2" "./$EXEC_BIN"
 fi
