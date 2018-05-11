c750221e6b10de5c22c98bc9e2b7c0aa15ff2b0c
Nigel Babu <nigelb@redhat.com>
2018-05-11 14:38:48 +0530

    add a new glusto job

--- scripts/glusto/run-glusto-main.sh
+++ scripts/glusto/run-glusto-main.sh
@@ -2,7 +2,7 @@
 # Run ansible script to setup everything
 
 # Get master IP
-host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
+host=$(grep ansible_host hosts | head -n 1 | awk '{split($2, a, "="); print a[2]}')
 
 # Build gluster packages
 #scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no scripts/glusto/build-rpms.sh root@${host}:build-rpms.sh
@@ -13,7 +13,7 @@ MAX=3
 RETRY=0
 while [ $RETRY -lt $MAX ];
 do
-    ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts scripts/glusto/setup-glusto.yml
+    ANSIBLE_HOST_KEY_CHECKING=False "$HOME/env/bin/ansible-playbook" -i hosts scripts/glusto/setup-glusto.yml
     RETURN_CODE=$?
     if [ $RETURN_CODE -eq 0 ]; then
         break
@@ -22,7 +22,15 @@ do
 done
 
 # run the test command from master
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no scripts/glusto/run-glusto.sh root@${host}:run-glusto.sh
-ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host EXIT_ON_FAIL=$EXIT_ON_FAIL ./run-glusto.sh -m $GLUSTO_MODULE
-JENKINS_STATUS=$?
-exit $JENKINS_STATUS
+if [ -z "$GLUSTO_PATCH" ]; then
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no scripts/glusto/run-glusto.sh "root@${host}:run-glusto.sh"
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "root@$host" EXIT_ON_FAIL="$EXIT_ON_FAIL" ./run-glusto.sh -m "$GLUSTO_MODULE"
+    JENKINS_STATUS=$?
+    exit $JENKINS_STATUS
+else
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no scripts/glusto/run-glusto-patch.sh "root@${host}:run-glusto.sh"
+    ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "root@$host" ./run-glusto-patch.sh -m "$GLUSTO_PATCH"
+    JENKINS_STATUS=$?
+    exit $JENKINS_STATUS
+
+fi
