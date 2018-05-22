86e179f0cbd8e6a3cd3828b04b04e46007f4fdec
Nigel Babu <nigelb@redhat.com>
2018-05-22 17:59:56 +0530

    Get the logs working again

--- scripts/glusto/get-glusto-logs.sh
+++ scripts/glusto/get-glusto-logs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set +e
 host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
+if [ -d "glusto" ]
+then
+  pushd centosci
+fi
 ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts scripts/glusto/get-gluster-logs.yml || true
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/root/gluster-logs.gz . || true
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/glustomain.log . || true
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/*junit.xml . || true
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/root/gluster-logs.gz $WORKSPACE/ || true
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/glustomain.log $WORKSPACE/ || true
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/*junit.xml $WORKSPACE || true
