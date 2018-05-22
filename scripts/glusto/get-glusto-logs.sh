8a56a24b51282261940dc2eba3860b0300c5a61e
Nigel Babu <nigelb@redhat.com>
2018-05-22 18:46:12 +0530

    Fix the clean up script again

--- scripts/glusto/get-glusto-logs.sh
+++ scripts/glusto/get-glusto-logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set +e
-host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
 if [ -d "glusto" ]
-then
-  pushd centosci
+    then
+    pushd centosci
 fi
+host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
 ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts scripts/glusto/get-gluster-logs.yml || true
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/root/gluster-logs.gz $WORKSPACE/ || true
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/glustomain.log $WORKSPACE/ || true
