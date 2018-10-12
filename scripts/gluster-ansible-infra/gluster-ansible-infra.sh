9c4d1f63faa36a332e9b5063ddc8c4f81f1bb2db
root <root@localhost.localdomain>
2018-10-12 17:51:59 +0530

    firewall-config-CI : CD line remove
    Signed-off-by: Sheersha Jain <sheershajain@gmail.com>

--- scripts/gluster-ansible-infra/gluster-ansible-infra.sh
+++ scripts/gluster-ansible-infra/gluster-ansible-infra.sh
@@ -3,7 +3,6 @@
 # basic dependencies for the tests
 yum -y install git
 git clone https://github.com/gluster/gluster-ansible-infra.git
-cd gluster-ansible-infra/roles/firewall_config/
 
 # Verify everything works with the following:
 docker run hello-world
@@ -15,4 +14,4 @@ then
     sudo yum install libselinux-python
 fi
 
-/bin/bash gluster-ansible-infra/tests/run-centos-ci.sh 
+./gluster-ansible-infra/tests/run-centos-ci.sh
