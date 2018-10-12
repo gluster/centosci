f436b7e6de628ec0282654e321480e9f8fcfcd70
root <root@localhost.localdomain>
2018-10-12 18:06:04 +0530

    firewall-config-CI : removed doc lines
    Signed-off-by: Sheersha Jain <sheershajain@gmail.com>

--- scripts/gluster-ansible-infra/gluster-ansible-infra.sh
+++ scripts/gluster-ansible-infra/gluster-ansible-infra.sh
@@ -4,14 +4,4 @@
 yum -y install git
 git clone https://github.com/gluster/gluster-ansible-infra.git
 
-# Verify everything works with the following:
-docker run hello-world
-
-# if selinux is enabled
-selinuxenabled
-if [ $? -ne 0 ]
-then
-    sudo yum install libselinux-python
-fi
-
 ./gluster-ansible-infra/tests/run-centos-ci.sh
