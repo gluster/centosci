ce5d2cea078ab38f22a76524e92e623094bbfc19
root <root@localhost.localdomain>
2018-10-12 11:17:33 +0530

    firewall-config-CI : script file changes
    Signed-off-by: Sheersha Jain <sheershajain@gmail.com>

--- scripts/gluster-ansible-infra/gluster-ansible-infra.sh
+++ scripts/gluster-ansible-infra/gluster-ansible-infra.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# basic dependencies for the tests
+yum -y install git
+git clone https://github.com/gluster/gluster-ansible-infra.git
+cd gluster-ansible-infra/roles/firewall_config/
+
 # Verify everything works with the following:
 docker run hello-world
 
@@ -8,5 +13,6 @@ selinuxenabled
 if [ $? -ne 0 ]
 then
     sudo yum install libselinux-python
+fi
 
-/bin/bash gluster-ansible-infra/tests 
+/bin/bash gluster-ansible-infra/tests/run-centos-ci.sh 
