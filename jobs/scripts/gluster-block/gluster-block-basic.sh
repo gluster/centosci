418205b732969adbc92d55d4299183620d746ae3
Amar Tumballi <amarts@redhat.com>
2018-10-18 16:50:41 +0530

    gluster-block-basic: add a test to build the gluster-block rpms.
    This also pushes RPMs to centos repo.
    
    Signed-off-by: Amar Tumballi <amarts@redhat.com>

---
+++ jobs/scripts/gluster-block/gluster-block-basic.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+GIT_BRANCH=${GIT_BRANCH:-master}
+CENTOS_VERSION=${CENTOS_VERSION:-7}
+CENTOS_ARCH=${CENTOS_ARCH:-x86_64}
+
+REPO_DIR=gluster-block/nightly
+REPO_NAME='gluster-block'
+REPO_VERSION='master'
+RSYNC_DIR=gluster/gluster-block-nightly
+
+artifact()
+{
+    [ -e ~/rsync.passwd ] || return 0
+    rsync -av --password-file ~/rsync.passwd "${@}" gluster@artifacts.ci.centos.org::${RSYNC_DIR}/
+}
+
+
+### Now build The rpms.
+# install basic dependencies for building the tarball and srpm
+install_dependency()
+{
+    yum -y install git autoconf automake gcc libtool bison flex
+    yum -y install make cmake rpm-build createrepo_c
+    # gluster repositories contain additional -devel packages
+    yum -y install centos-release-gluster
+    yum -y install libuuid-devel targetcli glusterfs-api-devel
+}
+
+clone_and_build_rpms()
+{
+    # TCMU-RUNNER
+    rm -rf tcmu-runner/
+    git clone https://github.com/open-iscsi/tcmu-runner
+
+    pushd tcmu-runner
+    cd extra/
+    bash make_runnerrpms.sh --without rbd --without qcow --without zbc --without fbo
+    rpm -i rpmbuild/RPMS/x86_64/tcmu-runner*.rpm
+    popd
+
+
+    rm -rf gluster-block/
+    git clone --depth 2 https://github.com/gluster/gluster-block/
+
+    pushd gluster-block
+    ./autogen.sh
+    ./configure
+    make rpms
+    popd
+
+}
+
+push_rpms_to_repo()
+{
+    TARGET_DIR="/srv/$REPO_DIR"
+    mkdir -p $TARGET_DIR/master
+
+    cp -a tcmu-runner/extra/rpmbuild/* $TARGET_DIR/master/
+    cp -a gluster-block/build/rpmbuild/* $TARGET_DIR/master/
+
+    pushd "$TARGET_DIR"
+    createrepo_c .
+    popd
+
+    cat > "${TARGET_DIR}/${REPO_NAME}.repo" <<< "[gluster-block-nightly-${REPO_VERSION}]
+name=Gluster Block Nightly builds (master branch)
+baseurl=http://artifacts.ci.centos.org/${RSYNC_DIR}/master/\$releasever/\$basearch
+enabled=1
+gpgcheck=0"
+
+    pushd "$TARGET_DIR"
+    artifact "${REPO_NAME}.repo"
+    artifact "master"
+    popd
+}
+
+main()
+{
+    install_dependency
+    clone_and_build_rpms
+
+    # Copy RPMS
+    # Sync it to relevant server
+
+    push_rpms_to_repo
+}
+
+main "$@"
