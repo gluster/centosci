8bb08309c225545c99b10174d69ddcf37eb10075
Deepshikha Khandelwal <dkhandel@redhat.com>
2019-05-03 12:53:51 +0530

    Add jenkins job to run lcov on gluster-block nightly (#58)
    Signed-off-by: Deepshikha khandelwal <dkhandel@redhat.com>

---
+++ jobs/scripts/gluster-block/lcov.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+yum install -y git autoconf automake gcc libtool make file rpcgen glusterfs-api-devel libuuid-devel json-c-devel libtirpc-devel
+
+# install runtime dependencies
+yum install -y glusterfs-server targetcli tcmu-runner
+
+# get the gluster-block source code
+rm -rf gluster-block/
+git clone --depth 2 https://github.com/gluster/gluster-block/
+
+nproc=$(getconf _NPROCESSORS_ONLN)
+
+# compile and istall from source code with lcov
+pushd gluster-block
+./autogen.sh
+./configure CFLAGS="-g3 -O0 -lgcov --coverage -fprofile-arcs -ftest-coverage"
+make install -j ${nproc}
+
+echo "Initializing the line coverage"
+mkdir coverage
+lcov -d . --zerocounters
+lcov -i -c -d . -o coverage/gluster-block-lcov.info
+set +e
+
+# start glusterd process
+systemctl start glusterd
+
+# run the test cases
+./tests/basic.t
+TEST_STATUS=$?
+
+echo "Capturing the line coverage in the .info file"
+lcov -c -d . -o coverage/gluster-block-lcov.info
+sed -i.bak '/stdout/d' coverage/gluster-block-lcov.info
+
+#Generating the html page for code coverage details using genhtml
+genhtml -o coverage/ coverage/gluster-block-lcov.info
+echo "The HTML report is generated as index.html file"
+popd
+
+if [ $TEST_STATUS -ne 0 ];
+    then
+    echo "Tests failed"
+    exit 1
+fi
