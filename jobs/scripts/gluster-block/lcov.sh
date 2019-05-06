c203ccb55696b5060fcfdf1a8885981fdf995af2
Deepshikha khandelwal <dkhandel@redhat.com>
2019-05-06 12:39:40 +0530

    Source install runtime dependencies: - targetcli-fb - stlib-fb - configshell-fb - tcmu-runner
    Signed-off-by: Deepshikha khandelwal <dkhandel@redhat.com>

--- jobs/scripts/gluster-block/lcov.sh
+++ jobs/scripts/gluster-block/lcov.sh
@@ -5,11 +5,30 @@ set -e
 yum install -y git autoconf automake gcc libtool make file glusterfs-api-devel libuuid-devel json-c-devel libtirpc-devel glibc-common
 
 # get epel and install lcov
-yum install -y epel-release
-yum install -y lcov
+yum install -y epel-release centos-release-gluster
+yum install -y lcov glusterfs-server
 
-# install runtime dependencies
-yum install -y glusterfs-server targetcli tcmu-runner
+# source code install other centos dependencies
+git clone https://github.com/open-iscsi/targetcli-fb
+cd targetcli-fb/
+./setup.py install
+cd ..
+
+git clone https://github.com/open-iscsi/rtslib-fb
+cd rtslib-fb
+./setup.py install
+cd ..
+
+git clone https://github.com/open-iscsi/configshell-fb
+cd configshell-fb
+./setup.py install
+cd ..
+
+git clone https://github.com/open-iscsi/tcmu-runner
+cd tcmu-runner
+yum install -y cmake make gcc libnl3 glib2 zlib kmod libnl3-devel glib2-devel zlib-devel kmod-devel
+cmake -DSUPPORT_SYSTEMD=ON -DCMAKE_INSTALL_PREFIX=/usr -Dwith-rbd=false -Dwith-qcow=false -Dwith-zbc=false -Dwith-fbo=false
+make install
 
 # get the gluster-block source code
 rm -rf gluster-block/
