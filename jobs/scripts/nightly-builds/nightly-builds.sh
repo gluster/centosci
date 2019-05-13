gluster/centosci

commit b552df317a130fd2ff12e76ea79c25e53f42e50f

git clone with just depth 1 for glusterfs (#57)
Signed-off-by: Amar Tumballi <amarts@redhat.com>

jobs/scripts/nightly-builds/nightly-builds.sh
@@ -17,7 +17,7 @@ yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-d
 
 # clone the repository, github is faster than our Gerrit
 #git clone https://review.gluster.org/glusterfs
-git clone https://github.com/gluster/glusterfs
+git clone --depth 1 https://github.com/gluster/glusterfs
 cd glusterfs/
 
 # switch to the branch we want to build

commit 98eefe13211da18ead743ea66332f2508407a112

nightly-builds: fix shallow cloning of git repository for non-master branches
When passing `--depth 1` to `git clone`, only the last commit of the
master branch will be fetched. Changing branches afterwards does not
work, as no other branches have been downloaded.

In order to do nightly builds for non-master branches, the `--branch`
argument to `git clone` is required. With this, it is not needed anymore
to checkout the branch afterwards.

Fixes: b552df317a1 ("git clone with just depth 1 for glusterfs")
Signed-off-by: Niels de Vos <ndevos@redhat.com>

jobs/scripts/nightly-builds/nightly-builds.sh
@@ -17,11 +17,8 @@ yum -y install python-devel libaio-devel librdmacm-devel libattr-devel libxml2-d
 
 # clone the repository, github is faster than our Gerrit
 #git clone https://review.gluster.org/glusterfs
-git clone --depth 1 https://github.com/gluster/glusterfs
+git clone --depth 1 --branch ${GERRIT_BRANCH} https://github.com/gluster/glusterfs
 cd glusterfs/
-
-# switch to the branch we want to build
-git checkout ${GERRIT_BRANCH}
 
 # generate a version based on branch.date.last-commit-hash
 if [ ${GERRIT_BRANCH} = 'master' ]; then