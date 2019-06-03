1a6821ae21259698cd85add4d86a43f4070bf4eb
Deepshikha Khandelwal <dkhandel@redhat.com>
2019-06-03 17:49:04 +0530

    Add centosci job for running clang-scan on gluster block (#66)
    * Add centosci job for running clang-scan on gluster block
    * Add option for clang-scan to view reports on the browser
    
    Signed-off-by: Deepshikha khandelwal <dkhandel@redhat.com>

---
+++ jobs/scripts/gluster-block/clang-scan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False"
+
+$MOCK --init
+# get epel and install packages
+$MOCK --install epel-release
+$MOCK --install git cmake libnl3 glib2 zlib kmod libnl3-devel glib2-devel zlib-devel kmod-devel autoconf automake gcc libtool make file glusterfs-api-devel libuuid-devel json-c-devel libtirpc-devel glibc-common python-setuptools clang clang-analyzer
+
+# source code install other centos dependencies
+$MOCK --chroot "git clone https://github.com/open-iscsi/targetcli-fb && ./targetcli-fb/setup.py install"
+
+$MOCK --chroot "git clone https://github.com/open-iscsi/rtslib-fb && ./rtslib-fb/setup.py install && cp ./rtslib-fb/systemd/target.service /usr/lib/systemd/system/target.service"
+
+$MOCK --chroot "git clone https://github.com/open-iscsi/configshell-fb && ./configshell-fb/setup.py install"
+
+$MOCK --chroot "git clone https://github.com/open-iscsi/tcmu-runner && cd tcmu-runner && cmake -DSUPPORT_SYSTEMD=ON -DCMAKE_INSTALL_PREFIX=/usr -Dwith-rbd=false -Dwith-qcow=false -Dwith-zbc=false -Dwith-fbo=false && make install && cd .."
+
+$MOCK --chroot "git clone --depth 2 https://github.com/gluster/gluster-block/ && ./gluster-block/autogen.sh && ./gluster-block/configure CC=clang --enable-gnfs --enable-debug"
+
+$MOCK --chroot "scan-build -V -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
+
+$MOCK --clean
