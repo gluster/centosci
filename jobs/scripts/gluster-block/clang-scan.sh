6799904c0d43958ec675246c80d07d05cbfb5826
Deepshikha khandelwal <dkhandel@redhat.com>
2019-06-24 16:45:29 +0530

    Run clang-analyzer for gluster-block
    Signed-off-by: Deepshikha khandelwal <dkhandel@redhat.com>

--- jobs/scripts/gluster-block/clang-scan.sh
+++ jobs/scripts/gluster-block/clang-scan.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-# install mock on the node
-yum install -y mock
-MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False"
+set -e
 
-$MOCK --init
-# get epel and install packages
-$MOCK --install epel-release
-$MOCK --install git cmake libnl3 glib2 zlib kmod libnl3-devel glib2-devel zlib-devel kmod-devel autoconf automake gcc libtool make file glusterfs-api-devel libuuid-devel json-c-devel libtirpc-devel glibc-common python-setuptools clang clang-analyzer
+yum install -y git autoconf automake gcc libtool make file glusterfs-api-devel libuuid-devel json-c-devel libtirpc-devel glibc-common python-setuptools clang clang-analyzer
+
+# get epel and install lcov
+yum install -y epel-release centos-release-gluster
+yum install -y glusterfs-server
 
 # source code install other centos dependencies
-$MOCK --chroot "git clone https://github.com/open-iscsi/targetcli-fb && ./targetcli-fb/setup.py install"
+git clone https://github.com/open-iscsi/targetcli-fb
+cd targetcli-fb/
+./setup.py install
+cd ..
 
-$MOCK --chroot "git clone https://github.com/open-iscsi/rtslib-fb && ./rtslib-fb/setup.py install && cp ./rtslib-fb/systemd/target.service /usr/lib/systemd/system/target.service"
+git clone https://github.com/open-iscsi/rtslib-fb
+cd rtslib-fb
+./setup.py install
+cp systemd/target.service /usr/lib/systemd/system/target.service
+cd ..
 
-$MOCK --chroot "git clone https://github.com/open-iscsi/configshell-fb && ./configshell-fb/setup.py install"
+git clone https://github.com/open-iscsi/configshell-fb
+cd configshell-fb
+./setup.py install
+cd ..
 
-$MOCK --chroot "git clone https://github.com/open-iscsi/tcmu-runner && cd tcmu-runner && cmake -DSUPPORT_SYSTEMD=ON -DCMAKE_INSTALL_PREFIX=/usr -Dwith-rbd=false -Dwith-qcow=false -Dwith-zbc=false -Dwith-fbo=false && make install && cd .."
+git clone https://github.com/open-iscsi/tcmu-runner
+cd tcmu-runner
+yum install -y cmake make gcc libnl3 glib2 zlib kmod libnl3-devel glib2-devel zlib-devel kmod-devel
+cmake -DSUPPORT_SYSTEMD=ON -DCMAKE_INSTALL_PREFIX=/usr -Dwith-rbd=false -Dwith-qcow=false -Dwith-zbc=false -Dwith-fbo=false
+make install
 
-$MOCK --chroot "git clone --depth 2 https://github.com/gluster/gluster-block/ && ./gluster-block/autogen.sh && ./gluster-block/configure CC=clang --enable-gnfs --enable-debug"
+# get the gluster-block source code
+rm -rf gluster-block/
+git clone --depth 2 https://github.com/gluster/gluster-block/
+pushd gluster-block
 
-$MOCK --chroot "scan-build -V -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
-
-$MOCK --clean
+scan-build -V -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make
