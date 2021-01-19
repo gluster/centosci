bb15d3ca0d840fc5df94840a2267c302092e5d2b
Kshithij Iyer <kshithij.ki@gmail.com>
2021-01-19 17:15:05 +0530

    Correct path for get-gluster-logs.yml
    Problem:
    The path to get-gluster-logs.yml  is missing jobs at the start of the path due to which it fails during runs with the below error:
    ```
    16:16:16 ++ /home/gluster/env/bin/ansible-playbook -i hosts scripts/glusto/get-gluster-logs.yml
    16:16:17 ERROR! the playbook: scripts/glusto/get-gluster-logs.yml could not be fou
    ```
    
    Fix:
    Adding `jobs` at the start of the path.

--- jobs/scripts/glusto/get-glusto-logs.sh
+++ jobs/scripts/glusto/get-glusto-logs.sh
@@ -6,7 +6,7 @@ if [ -d "glusto" ]
     pushd centosci
 fi
 host=$(cat hosts | grep ansible_host | head -n 1 | awk '{split($2, a, "="); print a[2]}')
-ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts scripts/glusto/get-gluster-logs.yml || true
+ANSIBLE_HOST_KEY_CHECKING=False $HOME/env/bin/ansible-playbook -i hosts jobs/scripts/glusto/get-gluster-logs.yml || true
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/root/gluster-logs.gz $WORKSPACE/ || true
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/glustomain.log $WORKSPACE/ || true
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$host:/tmp/*junit.xml $WORKSPACE || true
