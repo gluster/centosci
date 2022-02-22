5c05d40b1094aea78798ea724a0f3e2185cff60c
Rakshitha Kamath <rkamath@redhat.com>
2022-02-22 18:18:47 +0530

    1. Jenkins jobs migrated from ci.centos.org to new OCP4 instance 2. Made changes to run jobs on the node cico-workspace
    Signed-off-by: Rakshitha Kamath <rkamath@redhat.com>

--- jobs/scripts/common/rsync.sh
+++ jobs/scripts/common/rsync.sh
@@ -1,5 +1,4 @@
 set +x
-rsync_pass="$(cut -c1-13 < ~/duffy.key)"
+rsync_pass="$(echo $CICO_API_KEY | cut -c1-13)"
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) "echo $rsync_pass > ~/rsync.passwd"
 ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$(cat $WORKSPACE/hosts) "chmod 0600 ~/rsync.passwd"
-
--- tests/deploy-centos-ci.sh
+++ tests/deploy-centos-ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed "s/JENKINS_API_KEY/$JENKINS_API_KEY/g" tests/jenkins.conf > jobs/jenkins.ini
+sed "s/JENKINS_API_KEY/$JENKINS_API_KEY_NEW/g" tests/jenkins.conf > jobs/jenkins.ini
 jenkins-jobs --conf jobs/jenkins.ini update jobs
